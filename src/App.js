8397c480371d12aae1d1d63b332a2c4c6a9f192a
Cindy Zhu <zhucindy1234567890@gmail.com>
2023-05-16 17:50:35 -0400

    added article data

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/uni-news" element={<Section id="uni-news" header="UNIVERSITY NEWS" articles={university_news_articles} />} />
-        <Route path="/city-news" element={<Section id="city-news" header="CITY NEWS" articles={city_news_articles} />} />
         <Route path="/opinion" element={<Section id="opinion" header="OPINION" articles={opinion_articles} />} />
         <Route path="/a&c" element={<Section id="a&c" header="Arts and Culture" articles={ac_articles} />} />
         <Route path="/sports" element={<Section id="sports" header="SPORTS" articles={sports_articles} />} />
---
+++ src/data/article_data.js
@@ -0,0 +1,228 @@
+export const ac_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/arts-and-culture/2023/05/16/mfa-second-year-artists-make-a-statement-at-wallach-in-thesis-show/",
+        "article_title": "MFA second-year artists make a statement at Wallach in thesis show",
+        "article_authors": "Sinziana Stanciu",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/UOCHOSB4ZBAJXAJ2BOSINUG7NU.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/arts-and-culture/2023/05/14/sonic-star-steph-chow-cc-23-discusses-her-musical-journey-and-the-state-of-sound-in-the-streaming-era/",
+        "article_title": "Sonic star Steph Chow, CC ’23, discusses her musical journey and the state of sound in the streaming era",
+        "article_authors": "Madison Rosenfield",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/NTZZ5HTWVBB43KBWCTK3IKBYS4.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/arts-and-culture/2023/05/14/never-let-them-know-your-next-move-dj-and-visionary-dennis-franklin-cc-23-on-personal-growth-city-music-culture-and-individuality-through-artistic-expression/",
+        "article_title": "Never let them know your next move: DJ and visionary Dennis Franklin, CC ’23, on personal growth, city music culture, and individuality through artistic expression",
+        "article_authors": "Ariana Eftimiu and Madison Rosenfield",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/35ANUIRDSZEI3KVZWXPYNJN2FM.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/arts-and-culture/2023/05/14/caroline-sky-on-graduation-combining-music-and-advocacy-and-whats-next/",
+        "article_title": "Caroline Sky on graduation, combining music and advocacy, and what’s next",
+        "article_authors": "Ariana Eftimiu",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FRPUAJFDZRGR7DVKV6UBQLO54Y.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/arts-and-culture/2023/04/27/columbias-seniors-look-back-on-the-arts-at-columbia/",
+        "article_title": "Columbia’s seniors look back on the arts at Columbia",
+        "article_authors": "Liam Downey",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/HHBNLHLMORF4JH7YCR4WQLUP2A.jpg"
+    }
+];
+
+export const opinion_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/on-finding-and-losing-yourself-at-spec/",
+        "article_title": "On finding and losing yourself at Spec",
+        "article_authors": "Miles Schachner",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/3YWPRTWIXBEUBBNTJSFIPTYYKM.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/we-are-all-creative-writing-majors/",
+        "article_title": "We are all creative writing majors",
+        "article_authors": "Sofia Kwon",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ISX4AORUY5F3BCSR646P2WLW6Q.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/a-spectrum-of-identities/",
+        "article_title": "A spectrum of identities",
+        "article_authors": "Swethaa Suresh",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/IRFMR5OX2NCPBBVZUIH2NHJV54.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/the-case-for-the-anti-girlboss/",
+        "article_title": "The case for the anti-girlboss",
+        "article_authors": "Maya Mitrasinovic",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/H5QWLEHIIFGKNDTIISRTKE45MM.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/you-cant-make-everyone-happy-but-heres-to-trying/",
+        "article_title": "You can’t make everyone happy, but here’s to trying",
+        "article_authors": "Dia Gill",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZOMBRHFP2FHRLM2WRKBKGA3IWY.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/opinion/2023/05/15/why-spec-finding-and-building-community-in-riverside/",
+        "article_title": "Why Spec? Finding and building community in Riverside",
+        "article_authors": "Clara Ence Morse",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FAK3TUASWRCR3J2I6C3NEADYSE.jpg"
+    }
+];
+
+export const audio_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/podcasts/2023/05/05/one-touch-at-a-time-senior-fencer-teddy-lombardo-on-his-approach-as-mens-fencing-team-captain/",
+        "article_title": "‘One touch at a time:’ Senior fencer Teddy Lombardo on his approach as men’s fencing team captain.",
+        "article_authors": "Caleb Dye",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/LNJCVYO4XFA4ZJ7OQJVQ7CQEUA.jpg"
+    }
+];
+
+export const university_news_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/shomik-ghose-seas-23-discovers-the-power-of-finance-for-innovation/",
+        "article_title": "Shomik Ghose, SEAS ’23, discovers the power of finance for innovation",
+        "article_authors": "Cameron Spurr",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/KMXG2QB5WRGHTHS24TCSZR52AI.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/from-the-us-military-to-morningside-heights-duke-hopper-gs-23-reflects-on-time-at-columbia/",
+        "article_title": "From the US military to Morningside Heights: Duke Hopper, GS ’23, reflects on time at Columbia",
+        "article_authors": "John Towfighi",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ABWS3P44GZFLFNCFOJN7KCX75Y.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/grace-fox-cc-23-finds-identity-through-community-based-work-leads-fight-for-cser-departmentalization/",
+        "article_title": "Grace Fox, CC ’23, finds identity through community-based work, leads fight for CSER departmentalization",
+        "article_authors": "Amanda Chapa",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FSPCABVQVBC3JBGAER67L7MUMY.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/vanessa-feng-cc-23-reflects-on-paradigm-shifts-bridging-conversations-and-following-impulses/",
+        "article_title": "Vanessa Feng, CC ’23, reflects on paradigm shifts, bridging conversations, and following impulses",
+        "article_authors": "Ariel Yu",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/BR2DZAS6QFB43CYUXSVKCM2VWM.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/ananya-hajra-bc-23-on-experiencing-the-liberal-arts-and-greek-life-as-transfer-student/",
+        "article_title": "Ananya Hajra, BC ’23, on experiencing the liberal arts and Greek life as transfer student",
+        "article_authors": "Emily Forgash",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WBCDEYLYUBEDPPLM6XE4IUIYC4.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/james-harvey-elliot-ii-gs-23-on-growing-through-advocacy/",
+        "article_title": "James Harvey Elliot II, GS ’23, on growing through advocacy",
+        "article_authors": "Sabrina Ticer-Wurr",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/RCE4FX4OSZBRNEWWV5OUNDC54Q.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/karen-jang-bc-23-reflects-on-her-nonprofit-bracycle-research/",
+        "article_title": "Karen Jang, BC ’23, reflects on her nonprofit BraCycle, research",
+        "article_authors": "Maya Stahl and Caroline Capozzi",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/INQPYQDZO5GZZK4HFV33PGTCTU.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/angel-mancera-seas-23-on-school-politics-leaving-home-and-civic-duty/",
+        "article_title": "Angel Mancera, SEAS ’23, on school, politics, leaving home, and civic duty",
+        "article_authors": "Shea Vance",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/SCTIXWY3URB3BI2SG6LGORGEDY.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/charissa-ratliff-daddario-gs-23-founder-of-equality-for-gs-reflects-on-entering-advocacy-at-columbia/",
+        "article_title": "Charissa Ratliff D’addario, GS ’23, founder of Equality for GS, reflects on entering advocacy at Columbia",
+        "article_authors": "Gelila Negesse",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/6L2FQQ4ZO5BAFAYYGJGUXECQTM.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/charlie-wallace-cc-23-ccsc-president-reflects-on-leading-during-covid-and-a-life-long-interest-in-global-relations/",
+        "article_title": "Charlie Wallace, CC ’23, CCSC president, reflects on leading during COVID and a life-long interest in global relations",
+        "article_authors": "Apurva Chakravarthy",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/P4UE6KSKUBAYBCGGXK6ECSIWHI.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/trinity-sazo-seas-23-on-jade-finding-community-in-business-and-studying-computer-science/",
+        "article_title": "Trinity Sazo, SEAS ’23, on JADE, finding community in business, and studying computer science",
+        "article_authors": "Ayaan Ali",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/HBFJVBQZRZBTRBN5A3CQUFX5XE.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/tiffany-vo-reflects-on-sga-leadership-the-peahce-project-and-botany-research/",
+        "article_title": "Tiffany Vo, BC '23, reflects on SGA leadership, the Peahce Project, and botany research",
+        "article_authors": "Sarah Huddleston",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/S763DX7WK5GMLHZTEOZF3EUFAA.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/seas-celebrates-2023-class-day/",
+        "article_title": "SEAS celebrates 2023 Class Day",
+        "article_authors": "Rebecca Massel",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/NTS47LNENNE2VG5K45K6UTB5II.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/news/2023/05/15/school-of-general-studies-celebrates-2023-class-day/",
+        "article_title": "School of General Studies celebrates 2023 Class Day",
+        "article_authors": "Sarah Huddleston",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/OVVZLZ43JRDUVGGUREYB2YYVAE.jpg"
+    }
+];
+
+
+export const sports_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/sports/2023/05/16/im-gonna-go-out-and-show-that-im-the-best-how-a-fierce-competitive-spirit-made-kaitlyn-davis-one-of-the-most-decorated-players-columbia-womens-basketball-has-ever-seen/",
+        "article_title": "‘I'm gonna go out and show that I’m the best’: How a fierce competitive spirit made Kaitlyn Davis one of the most decorated players Columbia women’s basketball has ever seen",
+        "article_authors": "Jorge Hernandez",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/DZAUKHMYZNDZRN4JEHDPV6KTRY.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/sports/2023/05/16/mens-soccer-captain-max-gonzalez-reflects-on-columbia-career-and-journey/",
+        "article_title": "Men’s soccer captain, Max Gonzalez, reflects on Columbia career and journey",
+        "article_authors": "Takashi Williams",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGH5SJMWGFFMPBKG42WWYXQP7Q.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/sports/2023/05/16/it-gave-me-the-platform-to-really-bring-out-my-voice-ally-clark-and-her-columbia-soccer-journey/",
+        "article_title": "‘It gave me the platform to really bring out my voice’: Ally Clark and her Columbia soccer journey",
+        "article_authors": "Julian Silva-Forbes and Sydney Stoller",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/4LJAHD2P2VAVFMFQCXZWIYMZ2A.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/sports/2023/05/16/decorated-archery-senior-sachiko-keane-reflects-on-her-journey-through-columbia/",
+        "article_title": "Decorated Archery Senior Sachiko Keane reflects on her journey through Columbia",
+        "article_authors": "Richard Ren",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/LEKG72RPZFG57AIL7B72QNZN5A.jpg"
+    }
+];
+
+export const spectrum_articles = [
+    {
+        "article_link": "https://www.columbiaspectator.com/spectrum/2023/05/15/satire-reminiscing-on-your-firsts-at-columbia-for-the-class-of-2023/",
+        "article_title": "Satire: Reminiscing on your ‘firsts’ at Columbia for the class of 2023",
+        "article_authors": "Madeline Suindah",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/6DJ3PQEC7FH6DFBJ6HCQTGMNAU.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/spectrum/2022/09/19/open-when-youre-not-sure-what-the-next-step-is/",
+        "article_title": "Open when: You’re not sure what the next step is",
+        "article_authors": "Swethaa Suresh",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/RVBLJJQFRVGK7PMKOHQF2JNBYU.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/spectrum/2022/05/01/how-to-stay-close-to-barnard-columbia-community-after-graduation/",
+        "article_title": "How to stay close to the Barnard-Columbia community after graduation",
+        "article_authors": "Veronica Wang",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/S7X7MJFEKZCKFK6M3GMST4TL2I.JPG"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/spectrum/2023/05/15/items-to-cross-off-your-college-bucket-list-before-you-graduate/",
+        "article_title": "Items to cross off your college bucket list before you graduate",
+        "article_authors": "Ana Carpenter",
+        "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AA3W4YAF5RBODIRNOERB53FXKE.jpg"
+    },
+    {
+        "article_link": "https://www.columbiaspectator.com/spectrum/2020/02/26/open-when-you-dont-know-whats-next/",
+        "article_title": "Open when: you don’t know what’s next",
+        "article_authors": "Sydney Fox",
+        "image_url": "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/ZJHH242ECVF3FGDG4SF5O45AXY.jpg"
+    }
+];
